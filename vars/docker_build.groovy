1536b63715406b7f0f9c04fb8be10fa47107ccb4
DeVeNdRa0710 <devendra.k@webkorps.com>
2025-03-20 00:39:58 +0530

    Update docker_build.groovy
    Resolve typos

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,7 +1,7 @@
-def call(String DockerHubUser, String imageName, String imageTag){
+def call(String DockerHubUser, String ImageName, String ImageTag){
   
   timeout(time: 20, unit: 'MINUTES') {
-    sh "docker build -t ${DockerHubUser}/${name}:${tag} ."
+    sh "docker build -t ${DockerHubUser}/${ImageName}:${ImageTag} ."
   }
 
   echo 'Docker image built successfully'
