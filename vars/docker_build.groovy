7e70d5ac0ecc1b0427d114b03beba90f7b1da38f
DeVeNdRa0710 <devendra.k@webkorps.com>
2025-03-20 00:56:21 +0530

    Update docker_build.groovy
    try to use docker buildx

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,7 +1,7 @@
 def call(String DockerHubUser, String ImageName, String ImageTag){
   
   timeout(time: 20, unit: 'MINUTES') {
-    sh "docker build -t ${DockerHubUser}/${ImageName}:${ImageTag} ."
+    sh "docker buildx build --platform linux/amd64,linux/arm64 -t ${DockerHubUser}/${ImageName}:${ImageTag} --push ."
   }
 
   echo 'Docker image built successfully'
